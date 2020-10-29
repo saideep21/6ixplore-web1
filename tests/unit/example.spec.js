1dcac261dbabde8608aa0c2475d8631501c16de6
Maureen T'o <maureenito@gmail.com>
2020-10-29 09:02:00 -0400

    Add commented steps for next tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,6 +1,12 @@
 import { mount } from "@vue/test-utils";
 import navModal from "@/components/navModal.vue";
 
+// Three conditions for navModal component:
+/* 1. when openModal data is false, do not show navModal
+   2. when button burger icon clicked on, show navModal
+   3. when transparent background clicked, close navModal
+ */
+
 describe("navModal.vue", () => {
   it("If openModal is false, do not show .nav-modal", () => {
     const wrapper = mount(navModal);
